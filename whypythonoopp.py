e069658ec0dec020ea74d7a24228245324fc3b56
samreen <samreenss2414@gmail.com>
2025-06-01 08:24:37 +0530

    why python is object oriented done

--- whypythonoopp.py
+++ whypythonoopp.py
@@ -2,4 +2,4 @@ lst = [1,2,3]
 my_str = "mlops is intresting"
 my_int = 6778156
 
-print(type(lst)) #even though its asked to print it will give us class and object. hence python is object oriented programming
+print(type(lst)) #even though its asked to print it will give us class and object. hence python is object oriented program
