a9da197475065bef193e06c1c2e70c26f744b37c
samreen <samreenss2414@gmail.com>
2025-06-01 12:21:21 +0530

    inheritance and class explain in this

--- oops_proj.py
+++ oops_proj.py
@@ -1,28 +1,54 @@
-class ChatBook:
+class chatbook:
+
+    __user_id = 1
+
     def __init__(self):
-        self.username = ".."
-        self.password = ".."
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
+        self.__name = "Default User"
+        self.username = ''
+        self.password = ''
         self.loggedin = False
-        self.menu()
+        # self.menu()
+
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
+
+
+    def get_name(self):
+        return self.__name
+    
+
+    def set_name(self, value):
+        self.__name = value
 
 
     def menu(self):
-        user_input = input("""Welcome to chatbook... Please tell m e how can i help you...!
-                        1. Press 1 to signup
-                        2. press 2 to signin
-                        3. press 3 to write a post
-                        4. press 4 to meaasage ur friend
-                        5. press any key to exit""")
+        user_input = input(""""Welcome to Chatbook !! How would you like to proceed?
+                           1. Press 1 to signup
+                           2. Press 2 to signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit
+                           
+                           -> """)
         if user_input == "1":
-                        self.signup()
+            self.signup()
         elif user_input == "2":
-                        pass
+            self.signin()
         elif user_input == "3":
-                        pass
+            self.my_post()
         elif user_input == "4":
-                        pass
+            self.sendmsg()
         else:
             exit()
+
+
     def signup(self):
         email = input("enter your email here -> ")
         pwd = input("setup your password here -> ")
@@ -32,4 +58,37 @@ class ChatBook:
         print("\n")
         self.menu()
 
-obj = ChatBook()
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here -> ")
+            pwd = input("ENter your password here -> ")
+            if self.username==uname and self.password==pwd:
+                print("You have signed in successfully !!")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials..")
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            print(f"Following content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg? -> ")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
+
+# user1 = chatbook()
