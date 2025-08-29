400a749dcf158830276f94955a37528ddde9c44d
samreen <samreenss1424@gmail.com>
2025-08-29 06:54:41 +0530

    string functions added

--- basic.py
+++ basic.py
@@ -6,3 +6,32 @@ print(str[1])
 print(str[2:5])
 print(str[ :4])
 print(str[1:])
+
+#backward slicing
+print(str[-4:-1])
+
+#string functions 
+#1.ends with
+str1 = "I am learning python, it is easy"
+print(str1.endswith("sy"))
+# 2. starts with
+print(str1.startswith("I"))
+#3 capitalize
+print(str1.capitalize())
+#or
+str2 = str1.capitalize()
+print(str2)
+# replace function
+str3 = str1.replace("am", "was")
+print(str3)
+#4 find function
+print(str1.find("python"))
+#5 lower function
+print(str1.lower())
+#6 COunt function
+print(str1.count("a"))
+
+
+# WAP to input ur name and print the length of ur name
+name = input("Enter ur name : ")
+print("Length of ur name is : ", len(name))
